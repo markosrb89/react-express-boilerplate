36325762860ca5bcca9ac2d304412391978f5fd9
Marko Krtolica <markokrtolica1989@gmail.com>
2018-01-04 19:50:20 +0100

    updated

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.get("/api/customers", (req, res) => {
     const customers = [
-        {id: 1, firstName: 'Marko', lastName: 'Krtolica'},
-        {id: 2, firstName: 'Markony', lastName: 'Test'},
-        {id: 3, firstName: 'Plavic', lastName: 'Test1'},
+        {id: 1, firstName: 'Marko', lastName: 'Test'},
+        {id: 2, firstName: 'Markony', lastName: 'Test1'},
+        {id: 3, firstName: 'Plavic', lastName: 'Test2'},
       ];
 
       res.json(customers);
